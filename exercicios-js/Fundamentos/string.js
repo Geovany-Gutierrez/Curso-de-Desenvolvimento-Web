6918c0bd29dd7c030e847ff8333e7b5d29a6fc90
FalecidoGugu <gutigeovany@gmail.com>
2024-04-12 14:18:22 -0300

    Aprendendo sobre o Tipo String em JavaScript
    Entendendo algumas funçoes em string, como funcionam e exemplos

---
+++ exercicios-js/Fundamentos/string.js
@@ -0,0 +1,19 @@
+const escola = 'Cod3r'
+
+console.log(escola.charAt(4))
+console.log(escola.charAt(5))
+
+//charCodeAt é uma funçao que
+console.log(escola.charCodeAt(3))
+
+console.log(escola.indexOf('3'))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0,2))
+
+console.log('Escola'.concat(escola).concat('*'))
+console.log('Escola' + escola + '.')
+
+console.log(escola.replace(3, '2'))
+
+console.log('Ana, Maria, pedro'.split(','))
