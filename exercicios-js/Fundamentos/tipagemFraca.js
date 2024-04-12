f30e32526835d38a461402abbbe9735f34fd2968
FalecidoGugu <gutigeovany@gmail.com>
2024-04-12 13:47:20 -0300

    Tipagem
    Entendendo a tipagem do JavaScript

---
+++ exercicios-js/Fundamentos/tipagemFraca.js
@@ -0,0 +1,17 @@
+let qualquer = "Qualquer";
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 4;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = true;
+console.log(qualquer);
+console.log(typeof qualquer);
+/* Pela tipagem ser fraca, as alteraçoes acima sao possiveis */
+// Evitar nomes genericos e siglas nos identificadores 
+let valor = ''
+let numero = 0
+// Exemplos acima
+let nasc = '' //Exemplo de sigla desconhecida / Nascimento
