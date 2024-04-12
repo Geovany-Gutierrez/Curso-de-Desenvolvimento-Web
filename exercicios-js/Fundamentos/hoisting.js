4c96b87cfe6bb5b2d16514a7e37f20fcbe7efc02
FalecidoGugu <gutigeovany@gmail.com>
2024-04-12 16:33:14 -0300

    Hoisting em Js

---
+++ exercicios-js/Fundamentos/hoisting.js
@@ -0,0 +1,8 @@
+console.log('a = ' + a)
+var a = 2;
+console.log('a = ' + a)
+
+console.log('b: ' + b)
+let b = 2
+console.log('b: ' + b)
+// a declaraçao é movida para o topo ao usar var, fazendo com que uma funçao nao declarada(var a) seja undefined
